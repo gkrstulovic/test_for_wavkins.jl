bd43375d0ef6dba838bf8653c31ef2770d873dc8
Giorgio Krstulovic <krstulovic@oca.eu>
2024-11-09 23:33:10 +0100

    new test

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, test_for_wavkins
 
 
 makedocs(
-        sitename="Test docs fof WavKinS.jl",
+        sitename="Testing docs fof WavKinS.jl",
         modules=[test_for_wavkins],
         format=Documenter.HTML(prettyurls=true),
         remotes=nothing
