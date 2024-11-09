1cd4d6e2c77f059498a5a6729a26a310bf140328
Giorgio Krstulovic <krstulovic@oca.eu>
2024-11-09 22:36:09 +0100

    testing deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,10 @@
 using Documenter, test_for_wavkins
 
-makedocs(sitename="T")
+
+
+makedocs(
+        sitename="Test docs fof WavKinS.jl",
+        modules=[test_for_wavkins],
+        format=Documenter.HTML(prettyurls=true),
+        remotes=nothing
+)
